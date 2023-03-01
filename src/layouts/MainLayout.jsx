8c73aa638410012dfc8a2f43d78846a9effc5b41
hourglass2020 <pouria.fm15@gmail.com>
2023-03-01 15:15:26 +0330

    add mui to project

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import { ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+
 import { Outlet } from 'react-router-dom'
 
 import HeaderNav from '../components/navs/HeaderNav'
 
+import { theme } from "../components/ui/theme";
+import { Button } from "@mui/material";
+
+const cacheRTL = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin]
+})
+
 function MainLayout() {
     return (
-        <div>
-            <HeaderNav />
-            <main className='container'>
-                <Outlet />
-            </main>
-        </div>
+        <CacheProvider value={cacheRTL}>
+            <ThemeProvider theme={theme}>
+                <HelmetProvider>
+                    <Helmet>
+                        <title>گروه یاب</title>
+                    </Helmet>
+                    <HeaderNav />
+                    <main className='container'>
+                        <Outlet />
+                    </main>
+                </HelmetProvider>
+            </ThemeProvider>
+        </CacheProvider>
     )
 }
 
