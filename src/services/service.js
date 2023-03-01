d5cf765242cdad90d4af47ba9ec455e4a357f27c
hourglass2020 <pouria.fm15@gmail.com>
2023-03-01 13:37:20 +0330

    add all services

--- src/services/service.js
+++ src/services/service.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const SERVER_URL = "https://devgp.iranshomar.ir/api/v1";
 
+// ? Groups
+
 // @desc Get All Groups
 // @route GET %SERVER_URL%/groups
 export const getAllGroups = () => {
@@ -9,11 +11,9 @@ export const getAllGroups = () => {
     return axios.get(url);
 }
 
-// @desc Get All Tags
-// @route GET %SERVER_URL%/tags
-export const getAllTags = () => {
-    const url = `${SERVER_URL}/tags`;
-    return axios.get(url);
+export const addNewGroup = (groupData) => {
+    const url = `${SERVER_URL}/groups`;
+    return axios.post(url, groupData);
 }
 
 // @desc Get Specific group
@@ -28,5 +28,37 @@ export const getSpecificGroup = (groupSlug) => {
 export const getRelatedGroups = (groupSlug) => {
     const url = `${SERVER_URL}/groups/${groupSlug}/related`;
     return axios.get(url);
+}
+
+// ? Tags
+
+// @desc Get All Tags
+// @route GET %SERVER_URL%/tags
+export const getAllTags = () => {
+    const url = `${SERVER_URL}/tags`;
+    return axios.get(url);
+}
+
+// @desc Get a tag by slug
+// @route GET %SERVER_URL%/tags/:slug
+export const getAllTagsBySlug = (tagSlug) => {
+    const url = `${SERVER_URL}/tags/${tagSlug}`;
+    return axios.get(url);
+}
+
+// ? Comments
+
+// @desc Get the list of comments for a specific group
+// @route GET %SERVER_URL/comments?group={groupSlug}
+export const getGroupCommnets = (groupSlug) => {
+    const url = `${SERVER_URL}/comments?group=${groupSlug}`;
+    return axios.get(url);
+}
+
+// @desc Create new comment for a specific group
+// @route POST %SERVER_URL/comments?group={groupSlug}
+export const addNewComment = (commentData) => {
+    const url = `${SERVER_URL}/comments`;
+    return axios.post(url);
 }
 
